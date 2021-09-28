86a66ce12090125f138faa1051151d4cf23c876c
Lucas Batista <batista.lucas3098@gmail.com>
2021-09-28 13:02:33 -0300

    Estilização e criação do evento

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 // criar as sections com o DOM
 const parent = document.getElementById('estruturaGlobal');
 
-const section1 = document.createElement('section');
-parent.appendChild(section1);
+const torreInicial = document.createElement('section');
+torreInicial.id = "Torre-inicial"
+parent.appendChild(torreInicial);
 
-const section2 = document.createElement('section');
-parent.appendChild(section2);
+const torreMeio = document.createElement('section');
+torreMeio.id = "Torre-meio"
+parent.appendChild(torreMeio);
 
-const section3 = document.createElement('section');
-parent.appendChild(section3);
+const torreFinal = document.createElement('section');
+torreFinal.id = "Torre-final"
+parent.appendChild(torreFinal);
 
 // criar as divs com o DOM
-const div1 = document.createElement('div');
-section1.appendChild(div1);
+const disco1 = document.createElement('div');
+disco1.id = "disco1"
+torreInicial.appendChild(disco1);
 
-const div2 = document.createElement('div');
-section1.appendChild(div2);
+const disco2 = document.createElement('div');
+disco2.id = "disco2"
+torreInicial.appendChild(disco2);
 
-const div3 = document.createElement('div');
-section1.appendChild(div3);
+const disco3 = document.createElement('div');
+disco3.id = "disco3"
+torreInicial.appendChild(disco3);
 
 // criar um handler de clique em todas as sections
 // to achando que para essa parte e as que vêm a seguir precisaremos criar uma function. bora ver.
+
+const torres = document.querySelectorAll('section');
+console.log(torres);
+
+const disco = document.querySelectorAll('div');
+console.log(disco);
+
+torres.forEach((torre) => torre.addEventListener(
+    'click', (evt) => console.log(evt.target.id))
+);
